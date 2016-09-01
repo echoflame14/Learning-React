a5e16bb189aca3e9f5a92fcd4495eaaa4567e156
echoflame14 <joshpeterson.dev@gmail.com>
2016-09-01 01:09:40 -0600

    Finished with video 4 of tutorial

--- src/js/client.js
+++ src/js/client.js
@@ -1,17 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-class Layout extends React.Component {
-
-  render() {
-    return (
-      <div>
-        <h1>hey</h1>
-      </div>
-    );
-  }
-}
-
+import Layout from "./components/Layout.js";
 const app = document.getElementById("app");
-
-ReactDOM.render(<Layout/>, app);
+ReactDOM.render(< Layout / >, app);
---
+++ src/js/components/Header/Title.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default class title extends React.Component {
+
+	render() {
+		return (
+			<h1>Welcome</h1>
+		);
+	}
+}
