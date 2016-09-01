6075aa7935f2e2c8c9cc3d404e9a524fa1ffb77a
echoflame14 <joshpeterson.dev@gmail.com>
2016-08-31 21:22:42 -0600

    added babel

--- js/module2.js
+++ js/module2.js
@@ -1,1 +1,7 @@
 console.log("module2 stuff");
+let foo = {
+	bar: "numba1",
+	baz: "numbu2"
+};
+var [ bar ] = foo;
+console.log(bar);
--- webpack.config.js
+++ webpack.config.js
@@ -3,15 +3,20 @@ var webpack = require('webpack');
 
 module.exports = {
   context: __dirname,
+  module: {
+  loaders: [
+	 { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+  ]
+},
   devtool: debug ? "inline-sourcemap" : null,
   entry: "./js/script.js",
   output: {
-    path: __dirname + "/js",
-    filename: "script.min.js"
+	 path: __dirname + "/js",
+	 filename: "script.min.js"
   },
   plugins: debug ? [] : [
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+	 new webpack.optimize.DedupePlugin(),
+	 new webpack.optimize.OccurenceOrderPlugin(),
+	 new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
   ],
 };
